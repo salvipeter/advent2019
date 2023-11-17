596ce679f03981da12e067e5f1132add43439964
Peter Salvi <vukung@yahoo.com>
2023-11-17 11:07:31 +0100

    Day 11 in Forth

---
+++ adv11.fs
@@ -0,0 +1,129 @@
+create program
+  3 , 8 , 1005 , 8 , 330 , 1106 , 0 , 11 , 0 , 0 , 0 , 104 , 1 , 104 , 0 , 3 ,
+  8 , 102 , -1 , 8 , 10 , 1001 , 10 , 1 , 10 , 4 , 10 , 108 , 0 , 8 , 10 , 4 ,
+  10 , 1001 , 8 , 0 , 28 , 1 , 1103 , 17 , 10 , 1006 , 0 , 99 , 1006 , 0 , 91 ,
+  1 , 102 , 7 , 10 , 3 , 8 , 1002 , 8 , -1 , 10 , 101 , 1 , 10 , 10 , 4 , 10 ,
+  108 , 1 , 8 , 10 , 4 , 10 , 1002 , 8 , 1 , 64 , 3 , 8 , 102 , -1 , 8 , 10 ,
+  1001 , 10 , 1 , 10 , 4 , 10 , 108 , 0 , 8 , 10 , 4 , 10 , 102 , 1 , 8 , 86 ,
+  2 , 4 , 0 , 10 , 1006 , 0 , 62 , 2 , 1106 , 13 , 10 , 3 , 8 , 1002 , 8 , -1 ,
+  10 , 1001 , 10 , 1 , 10 , 4 , 10 , 1008 , 8 , 0 , 10 , 4 , 10 , 101 , 0 , 8 ,
+  120 , 1 , 1109 , 1 , 10 , 1 , 105 , 5 , 10 , 3 , 8 , 102 , -1 , 8 , 10 ,
+  1001 , 10 , 1 , 10 , 4 , 10 , 108 , 1 , 8 , 10 , 4 , 10 , 1002 , 8 , 1 , 149 ,
+  1 , 108 , 7 , 10 , 1006 , 0 , 40 , 1 , 6 , 0 , 10 , 2 , 8 , 9 , 10 , 3 , 8 ,
+  102 , -1 , 8 , 10 , 1001 , 10 , 1 , 10 , 4 , 10 , 1008 , 8 , 1 , 10 , 4 , 10 ,
+  1002 , 8 , 1 , 187 , 1 , 1105 , 10 , 10 , 3 , 8 , 102 , -1 , 8 , 10 , 1001 ,
+  10 , 1 , 10 , 4 , 10 , 1008 , 8 , 1 , 10 , 4 , 10 , 1002 , 8 , 1 , 213 ,
+  1006 , 0 , 65 , 1006 , 0 , 89 , 1 , 1003 , 14 , 10 , 3 , 8 , 102 , -1 , 8 ,
+  10 , 1001 , 10 , 1 , 10 , 4 , 10 , 108 , 0 , 8 , 10 , 4 , 10 , 102 , 1 , 8 ,
+  244 , 2 , 1106 , 14 , 10 , 1006 , 0 , 13 , 3 , 8 , 102 , -1 , 8 , 10 , 1001 ,
+  10 , 1 , 10 , 4 , 10 , 108 , 0 , 8 , 10 , 4 , 10 , 1001 , 8 , 0 , 273 , 3 ,
+  8 , 1002 , 8 , -1 , 10 , 1001 , 10 , 1 , 10 , 4 , 10 , 108 , 1 , 8 , 10 , 4 ,
+  10 , 1001 , 8 , 0 , 295 , 1 , 104 , 4 , 10 , 2 , 108 , 20 , 10 , 1006 , 0 ,
+  94 , 1006 , 0 , 9 , 101 , 1 , 9 , 9 , 1007 , 9 , 998 , 10 , 1005 , 10 , 15 ,
+  99 , 109 , 652 , 104 , 0 , 104 , 1 , 21102 , 937268450196 , 1 , 1 , 21102 ,
+  1 , 347 , 0 , 1106 , 0 , 451 , 21101 , 387512636308 , 0 , 1 , 21102 , 358 ,
+  1 , 0 , 1105 , 1 , 451 , 3 , 10 , 104 , 0 , 104 , 1 , 3 , 10 , 104 , 0 , 104 ,
+  0 , 3 , 10 , 104 , 0 , 104 , 1 , 3 , 10 , 104 , 0 , 104 , 1 , 3 , 10 , 104 ,
+  0 , 104 , 0 , 3 , 10 , 104 , 0 , 104 , 1 , 21101 , 0 , 97751428099 , 1 ,
+  21102 , 1 , 405 , 0 , 1105 , 1 , 451 , 21102 , 1 , 179355806811 , 1 , 21101 ,
+  416 , 0 , 0 , 1106 , 0 , 451 , 3 , 10 , 104 , 0 , 104 , 0 , 3 , 10 , 104 , 0 ,
+  104 , 0 , 21102 , 1 , 868389643008 , 1 , 21102 , 439 , 1 , 0 , 1105 , 1 ,
+  451 , 21102 , 1 , 709475853160 , 1 , 21102 , 450 , 1 , 0 , 1105 , 1 , 451 ,
+  99 , 109 , 2 , 22102 , 1 , -1 , 1 , 21101 , 0 , 40 , 2 , 21101 , 482 , 0 , 3 ,
+  21102 , 1 , 472 , 0 , 1105 , 1 , 515 , 109 , -2 , 2106 , 0 , 0 , 0 , 1 , 0 ,
+  0 , 1 , 109 , 2 , 3 , 10 , 204 , -1 , 1001 , 477 , 478 , 493 , 4 , 0 , 1001 ,
+  477 , 1 , 477 , 108 , 4 , 477 , 10 , 1006 , 10 , 509 , 1101 , 0 , 0 , 477 ,
+  109 , -2 , 2105 , 1 , 0 , 0 , 109 , 4 , 2101 , 0 , -1 , 514 , 1207 , -3 , 0 ,
+  10 , 1006 , 10 , 532 , 21101 , 0 , 0 , -3 , 21202 , -3 , 1 , 1 , 22101 , 0 ,
+  -2 , 2 , 21101 , 1 , 0 , 3 , 21101 , 0 , 551 , 0 , 1105 , 1 , 556 , 109 , -4 ,
+  2106 , 0 , 0 , 109 , 5 , 1207 , -3 , 1 , 10 , 1006 , 10 , 579 , 2207 , -4 ,
+  -2 , 10 , 1006 , 10 , 579 , 22102 , 1 , -4 , -4 , 1105 , 1 , 647 , 21201 ,
+  -4 , 0 , 1 , 21201 , -3 , -1 , 2 , 21202 , -2 , 2 , 3 , 21101 , 0 , 598 , 0 ,
+  1106 , 0 , 556 , 22101 , 0 , 1 , -4 , 21102 , 1 , 1 , -1 , 2207 , -4 , -2 ,
+  10 , 1006 , 10 , 617 , 21101 , 0 , 0 , -1 , 22202 , -2 , -1 , -2 , 2107 , 0 ,
+  -3 , 10 , 1006 , 10 , 639 , 22102 , 1 , -1 , 1 , 21102 , 1 , 639 , 0 , 105 ,
+  1 , 514 , 21202 , -2 , -1 , -2 , 22201 , -4 , -2 , -4 , 109 , -5 , 2105 , 1 ,
+  0 ,
+here program - cell / constant length
+
+variable x variable y variable dir
+: left  dir @ 1- 4 mod dir ! ;
+: right dir @ 1+ 4 mod dir ! ;
+: forward dir @ case
+    0 of -1 y +! endof
+    1 of  1 x +! endof
+    2 of  1 y +! endof
+    3 of -1 x +! endof
+  endcase ;
+
+variable npanels
+create panels 10000 cells allot
+: findpanel ( x y -- addr | 0 )
+  panels npanels @ 0 ?do \ x y addr
+    >r 2dup r@ cell+ @ = swap r@ @ = and if
+      2drop r> unloop exit
+    then r> 3 cells +
+  loop drop 2drop false ;
+: addpanel ( c -- ) 
+  x @ y @ findpanel ?dup if 2 cells + ! else
+    npanels @ 3 * cells panels + \ c addr
+    x @ over ! cell+ y @ over ! cell+ ! 1 npanels +!
+  then ;
+: color ( x y -- c ) findpanel dup 0<> if 2 cells + @ then ;
+
+variable turn
+: paint-or-turn ( b -- )
+  turn @ if
+    if right else left then forward false turn !
+  else
+    addpanel true turn !
+  then ;
+
+\ As before, except init/input/output
+variable relbase
+50000 constant memsize
+create memory memsize cells allot 
+: mem@ cells memory + @ ;
+: mem! cells memory + ! ;
+: init ( -- )
+  0 npanels ! 0 x ! 0 y ! 0 dir ! false turn !
+  memory memsize cells 0 fill
+  program memory length cells move 0 relbase ! ;
+: arg ( ip -- ip ip+1 ) dup 1+ ;
+: [arg] ( ip -- [ip] ip+1 ) dup mem@ swap 1+ ;
+: r[arg] ( ip -- r[ip] ip+1 ) dup mem@ relbase @ + swap 1+ ;
+: args ( ip m k -- a1 ... ak ip' )
+  rot 1+ -rot 0 ?do \ ip' m
+    10 /mod >r
+    dup 0= if drop [arg] else
+    2 = if r[arg] else
+    arg then then r>
+  loop drop ;
+\ Instructions ( ip m -- ip' )
+: add 3 args >r -rot mem@ swap mem@ + swap mem! r> ;
+: mul 3 args >r -rot mem@ swap mem@ * swap mem! r> ;
+: input 1 args >r x @ y @ color swap mem! r> ;
+: output 1 args >r mem@ paint-or-turn r> ;
+: jmptrue 2 args rot mem@ 0<> if drop mem@ else nip then ;
+: jmpfalse 2 args rot mem@ 0= if drop mem@ else nip then ;
+: less 3 args >r -rot mem@ swap mem@ > abs swap mem! r> ;
+: equals 3 args >r -rot mem@ swap mem@ = abs swap mem! r> ;
+: rebase 1 args >r mem@ relbase +! r> ;
+create instr
+  ' add , ' mul , ' input , ' output , ' jmptrue , ' jmpfalse ,
+  ' less , ' equals , ' rebase ,
+: call ( ip m n -- ip' ) 1- cells instr + @ execute ;
+: run ( -- )
+  0 begin
+    dup mem@ 100 /mod swap
+    dup 99 <> while
+    dup 9 > abort" Invalid opcode"
+    call
+  repeat 2drop drop ;
+
+: part1 init run npanels ? ;
+: paint
+  cr 6 0 do 40 1 do
+    i j color if [char] # else [char] . then emit 
+  loop cr loop ;
+: part2 init 1 addpanel run paint ;
